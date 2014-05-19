be92ff8e18d4cdb10a1d14450655aa56701506f4
Richard Feldman <richard.t.feldman@gmail.com>
2014-05-18 21:21:35 -0700

    Add some basic functions around mana costs

--- src/MTGSimulator.hs
+++ src/MTGSimulator.hs
@@ -1,4 +1,19 @@
 module MTGSimulator where
 
-data Mana = Black | Blue | Green | Red | White | Colorless | Hybrid Mana Mana
+import qualified Data.Map.Strict as Map
 
+data Mana = Black | Blue | Green | Red | White | Colorless | Hybrid Mana Mana deriving (Show, Eq, Ord)
+
+addToSymbolCount :: Map.Map Mana Int -> Mana -> Map.Map Mana Int
+addToSymbolCount counts symbol =
+  Map.insert symbol (currentCount + 1) counts
+  where currentCount = Map.findWithDefault 0 symbol counts
+
+countSymbols :: [Mana] -> Map.Map Mana Int
+countSymbols = foldl addToSymbolCount (Map.empty :: Map.Map Mana Int)
+
+castableWith :: [Mana] -> [Mana] -> Bool
+castableWith cost available =
+  True
+  where costCounts      = countSymbols cost
+        availableCounts = countSymbols available
