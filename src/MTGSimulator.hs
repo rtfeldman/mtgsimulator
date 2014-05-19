b4e44ae137192fc23a5fee995648539ac9af031e
Richard Feldman <richard.t.feldman@gmail.com>
2014-05-18 22:18:53 -0700

    Refactored some stuff to use conveniences.

--- src/MTGSimulator.hs
+++ src/MTGSimulator.hs
@@ -4,13 +4,21 @@ import qualified Data.Map.Strict as Map
 
 data Mana = Black | Blue | Green | Red | White | Colorless | Hybrid Mana Mana deriving (Show, Eq, Ord)
 
-addToSymbolCount :: Map.Map Mana Int -> Mana -> Map.Map Mana Int
-addToSymbolCount counts symbol =
-  Map.insert symbol (currentCount + 1) counts
-  where currentCount = Map.findWithDefault 0 symbol counts
+type ManaCounts = Map.Map Mana Int
 
-countSymbols :: [Mana] -> Map.Map Mana Int
-countSymbols = foldl addToSymbolCount (Map.empty :: Map.Map Mana Int)
+emptyManaCounts :: ManaCounts
+emptyManaCounts = Map.empty
+
+addToManaCounts :: ManaCounts -> Mana -> ManaCounts
+addToManaCounts counts symbol =
+  Map.insert symbol (currentCount + 1) counts
+  where currentCount = countBySymbol symbol counts
+
+countBySymbol :: Mana -> ManaCounts -> Int
+countBySymbol = Map.findWithDefault 0
+
+countSymbols :: [Mana] -> ManaCounts
+countSymbols = foldl addToManaCounts emptyManaCounts
 
 castableWith :: [Mana] -> [Mana] -> Bool
 castableWith cost available =
